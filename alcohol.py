8138f74b3f135a33bfb6d61a49444f8add88cb11
lukasbe95 <lukasbe@gazeta.pl>
2019-01-15 23:19:06 +0100

    Few methods for prediction with sklearn.

--- alcohol.py
+++ alcohol.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+merged_cols = ["school", "sex", "age", "address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob",
+                          "reason", "nursery", "internet"]
 
 class DataPreprocess(object):
-    def __init__(self):
-        self.__train_set = pd.read_csv("./student-por.csv",sep=";")
+    def __init__(self, file_path: str = None):
+        data_por = pd.read_csv("../student-por.csv", sep=";")
+        data_mat = pd.read_csv("./student-mat.csv", sep=';')
+        merged = pd.merge(data_por, data_mat, on=merged_cols)
+        # self.__train_set = pd.read_csv(file_path)
+        self.__train_set = merged
         self.__new_set = None
         self.__series = []
 
-    def parse_two(self, value_map, column_name):
+    def parse_two(self, value_map, column_name, new_name):
+        """
+
+        :param value_map:
+        :param column_name:
+        :return:
+        """
         new_series = pd.Series(
             [value_map[item] for item in self.__train_set[column_name]],
-            name=column_name)
+            name=new_name)
         self.__series.append(new_series)
 
     def value_map(self, column_name):
@@ -24,13 +40,23 @@ class DataPreprocess(object):
         value_set = []
         for item in self.__train_set[column_name]:
             value_set.append(item) if item not in value_set else None
-        for i in value_set:
+
+        if len(value_set) == 2:
+            value_one = value_set[0]
             data_for_new_series = []
             for x in self.__train_set[column_name]:
-                data_for_new_series.append(1) if x == i else data_for_new_series.append(0)
+                data_for_new_series.append(1) if x == value_one else data_for_new_series.append(0)
             tS = pd.Series(data=data_for_new_series,
-                      name=str(column_name+"_"+i))
+                           name=str(column_name + "_" + value_one))
             self.__series.append(tS)
+        else:
+            for i in value_set:
+                data_for_new_series = []
+                for x in self.__train_set[column_name]:
+                    data_for_new_series.append(1) if x == i else data_for_new_series.append(0)
+                tS = pd.Series(data=data_for_new_series,
+                          name=str(column_name+"_"+i))
+                self.__series.append(tS)
 
     def merge_series_to_dataframe(self):
         self.__new_set = pd.DataFrame(self.__series).transpose()
@@ -44,52 +70,58 @@ class DataPreprocess(object):
     def get_new_set(self):
         return self.__new_set
 
+    def parse_values_without_dict(self, column_name):
+        new_dict = {}
+        set_keys = set()
+        iter_value = 0
+        new_name = ""
+        [set_keys.add(item) for item in self.__train_set[column_name]]
+        for key in set_keys:
+            new_dict[key] = iter_value
+            iter_value = iter_value + 1
+        for k in new_dict.keys():
+            if new_dict[k] == 1:
+                new_name = "{col_name}_{key}".format(col_name=column_name, key=k)
+        if len(set_keys) == 2:
+            self.parse_two(new_dict, column_name, new_name)
+        else:
+            self.create_new_series_and_map(column_name)
+
     def perform(self):
-        self.parse_two({"GP":1, "MS":0}, "school")
-        self.parse_two({"F":1, "M":0}, "sex")
-        self.value_map("age")
-        self.parse_two({"U":1, "R":0}, "address")
-        self.parse_two({"GT3":1, "LE3":0}, "famsize")
-        self.parse_two({"T":1, "A":0}, "Pstatus")
-        self.value_map("Medu")
-        self.value_map("Fedu")
-        self.create_new_series_and_map("Mjob")
-        self.create_new_series_and_map("Fjob")
-        self.create_new_series_and_map("reason")
-        self.create_new_series_and_map("guardian")
-        self.value_map("traveltime")
-        self.parse_two({"yes":1, "no":0}, "schoolsup")
-        self.parse_two({"yes":1, "no":0}, "famsup")
-        self.parse_two({"yes":1, "no":0}, "paid")
-        self.parse_two({"yes":1, "no":0}, "activities")
-        self.parse_two({"yes":1, "no":0}, "nursery")
-        self.parse_two({"yes":1, "no":0}, "higher")
-        self.parse_two({"yes":1, "no":0}, "internet")
-        self.parse_two({"yes":1, "no":0}, "romantic")
-        self.value_map("famrel")
-        self.value_map("freetime")
-        self.value_map("goout")
-        self.value_map("Dalc")
-        self.value_map("Walc")
-        self.value_map("health")
-        self.value_map("absences")
-        self.value_map("G1")
-        self.value_map("G2")
-        self.value_map("G3")
+
+        non_numerical_cols = self.__train_set.select_dtypes(include=['object']).keys()
+
+        for key in self.__train_set.keys():
+            if key in non_numerical_cols:
+                self.parse_values_without_dict(key)
+            else:
+                self.value_map(key)
+
         self.merge_series_to_dataframe()
         self.dataframe_to_csv('whole_set.csv')
 
     @staticmethod
-    def split_x_y(data_set, which_g='G1'):
+    def split_x_y(data_set, which_g='G1_x'):
 
         x = data_set[:]
-        x.drop(['G1', 'G2', 'G3'], axis=1)
+        for key in x.keys():
+            if 'G' in key:
+                x.drop([key], axis=1)
         y = data_set[[which_g]]
 
         return x.as_matrix(), y.as_matrix()
 
 
 
-#
-# if __name__ == "__main__":
-#     main()
+
+if __name__ == "__main__":
+    preprocesor = DataPreprocess()
+    preprocesor.perform()
+    merged = preprocesor.get_new_set()
+    print(merged.shape)
+    with open('merger_dumped', 'w') as f:
+        f.write(merged.to_csv())
+
+    plt.figure(figsize=(25, 25))
+    sns.heatmap(merged.corr(), annot=True, fmt=".2f", cbar=True)
+    plt.show()
--- create_nn.py
+++ create_nn.py
@@ -1,78 +1,73 @@
 import tensorflow as tf
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, ARDRegression, ElasticNet, Lasso
+from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 from alcohol import DataPreprocess
 
+def RandomForest_train_and_plot(x_train, x_test, y_train, y_test):
+    reg = RandomForestRegressor(max_depth=2, random_state=0,
+                                n_estimators=100).fit(x_train[:], y_train[:])
+    reg.score(x_train[:], y_train[:])
+    predicted_y = reg.predict(x_test[:])
+    pred_y_transposed = np.array([[x] for x in predicted_y])
+    print(y_test[:] - pred_y_transposed)
+    sns.distplot(y_test[:] - pred_y_transposed)
+    plt.show()
 
-preprocesor = DataPreprocess()
-preprocesor.perform()
-dataset = preprocesor.get_new_set()
+def linear_reg_train_and_plot(x_train, x_test, y_train, y_test):
+    reg = LinearRegression().fit(x_train[:], y_train[:])
+    reg.score(x_train[:], y_train[:])
+    predicted_y = reg.predict(x_test[:])
+    print(y_test[:] - predicted_y)
+    sns.distplot(y_test[:] - predicted_y)
+    plt.show()
 
+def Lasso_train_and_plot(x_train, x_test, y_train, y_test):
+    reg = Lasso(alpha = .1).fit(x_train[:], y_train[:])
+    reg.score(x_train[:], y_train[:])
+    predicted_y = reg.predict(x_test[:])
+    pred_y_transposed = np.array([[x] for x in predicted_y])
+    print(y_test[:] - pred_y_transposed)
+    sns.distplot(y_test[:] - pred_y_transposed)
+    plt.show()
 
-'''
-TODO
-- divide data set to: train, validation and test set (method!)
-- build network
-- run and test
-'''
-# TODO - complete/implement:
-# train_set, validation_set, test_set = some_split_set_method(dataset)
+# ElasticNet_train_and_plot
+def ARDR_train_and_plot(x_train, x_test, y_train, y_test):
+    reg = ARDRegression().fit(x_train[:], y_train[:])
+    reg.score(x_train[:], y_train[:])
+    predicted_y = reg.predict(x_test[:])
+    pred_y_transposed = np.array([[x] for x in predicted_y])
+    print(y_test[:] - pred_y_transposed)
+    sns.distplot(y_test[:] - pred_y_transposed)
+    plt.show()
 
-trainX, trainY = DataPreprocess.split_x_y(train_set)
+def ElasticNet_train_and_plot(x_train, x_test, y_train, y_test):
+    reg = ElasticNet(alpha = .5, l1_ratio=0.5).fit(x_train[:], y_train[:])
+    reg.score(x_train[:], y_train[:])
+    predicted_y = reg.predict(x_test[:])
+    pred_y_transposed = np.array([[x] for x in predicted_y])
+    print(y_test[:] - pred_y_transposed)
+    sns.distplot(y_test[:] - pred_y_transposed)
+    plt.show()
 
-# Parameters
-learning_rate = 0.000001
-training_epochs = 2000
-display_step = 50
-n_samples = trainY.size
+if __name__ == '__main__':
+    preprocesor = DataPreprocess()
+    preprocesor.perform()
+    dataset = preprocesor.get_new_set()
+    logs_path = "/tmp/tesorflow_logs/example/"
+    datasetX, datasetY  = DataPreprocess.split_x_y(dataset)
+    x_train, x_test, y_train, y_test = train_test_split(datasetX, datasetY, test_size=0.2)
+    x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5)
 
-n_parameters = trainX.size[0]
-
-
-#########################        building NN structure:       ########################################
-
-x = tf.placeholder(tf.float32, [None, n_parameters])  # input where we will feed one row at a time
-
-W = tf.Variable(tf.zeros([n_parameters, 1]))     # Weights matrix
-
-b = tf.Variable(tf.zeros([n_parameters]))        # Biases - nie wiem po co ale są
-
-y = tf.nn.softmax(tf.add(tf.matmul(x, W), b))       # NN output
-
-# to train network we will need a reference output to construct cost function
-y_ref = tf.placeholder(tf.float32, [None, 1])  #placeholder for trainY, (only one value -> predicted grade)
-
-# Cost function: Mean squared error# Cost fu
-cost = tf.reduce_sum(tf.pow(y_ref - y, 2))/(2*n_samples)
-# Gradient descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-
-################3       Initialize variables and tensorflow session    ####################333
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-
-
-for i in range(training_epochs):
-    sess.run(optimizer, feed_dict={x: trainX, y_ref: trainY}) # Take a gradient descent step using our inputs and labels
-
-    if i % display_step == 0:
-        cc = sess.run(cost, feed_dict={x: trainX, y_ref: trainY})
-        print("Training step:", '%04d' % i, "cost=", "{:.9f}".format(cc))
-
-        '''
-        to get current weights:
-        current_W = sess.run(W)
-        '''
-
-print("Optimization Finished!")
-training_cost = sess.run(cost, feed_dict={x: trainX, y_ref: trainY})
-print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
-
-
-# TODO : How to Validate?
-
-# TODO : Run session with testX, get output Y and comare it with testY
-# something like:
-# y = sess.run(y, feed_dict={x: testX})
+    # methods to choose
+    
+    # linear_reg_train_and_plot(x_train, x_test, y_train, y_test)
+    # ARDR_train_and_plot(x_train, x_test, y_train, y_test)
+    # Lasso_train_and_plot(x_train, x_test, y_train, y_test)
+    # ElasticNet_train_and_plot(x_train, x_test, y_train, y_test)
+    RandomForest_train_and_plot(x_train, x_test, y_train, y_test)
